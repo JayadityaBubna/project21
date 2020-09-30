00d702fddaaa0daba7c8004b52850883371c5642
JayadityaBubna <70836398+JayadityaBubna@users.noreply.github.com>
2020-09-30 19:06:30 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master (3)/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,55 @@
+var bullet,wall,thickness,weight,speed,damage
+
+function setup() {
+ createCanvas(1600,400);
+ bullet= createSprite(200, 200, 10, 5);
+ bullet.shapeColor="blue";
+ thickness=random(22,83);
+ wall=createSprite(1200,200,thickness,height/2);
+ wall.shapeColor=rgb(80,80,80);
+ weight=random(30,52);
+ speed=random(223,321);
+ bullet.velocityX=speed;
+
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  if(collide(bullet,wall)){
+
+    bullet.velocityX=0;
+    var damage= 0.5* weight* speed* speed/ (thickness* thickness* thickness) ;
+  }
+
+  if(damage>10){
+    wall.shapeColor="red"
+
+  }
+   
+  if(damage<10){
+    wall.shapeColor="green"
+
+  }
+ 
+  
+  
+  drawSprites();
+}
+
+
+
+function collide(object1,object2){
+
+  bulletRightEdge=object1.x + object1.width ;
+  wallLeftEdge=object2.x;
+
+  if(bulletRightEdge>=wallLeftEdge){
+   return true;
+  }
+  
+  else{
+   return false;
+  }
+
+}
